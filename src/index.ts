14564b18ef818b5164731aaa8ee22b9a9f0e78c0
Will Downey <willd20@vt.edu>
2022-07-09 20:04:48 -0400

    Create and insert post row

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello there");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from './constants';
+import { Post } from "./entities/Post";
+import microConfig from './mikro-orm.config';
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig);
+
+    const post = orm.em.create(Post, {title: 'my first post'} as {title: string, createdAt: Date, updatedAt: Date});
+    await orm.em.persistAndFlush(post);
+    console.log('sql2--------');
+    await orm.em.nativeInsert(Post, {title:'my first post 2'})
+}
+
+main().catch((err) => {
+    console.error(err);
+});
