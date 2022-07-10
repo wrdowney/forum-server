96b840b334287cd89052795097ee406b0cf88c65
Will Downey <willd20@vt.edu>
2022-07-09 21:29:32 -0400

    Optional properties

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,18 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Entity, OptionalProps, PrimaryKey, Property } from "@mikro-orm/core";
 
 @Entity()
 export class Post {
+    [OptionalProps]?: 'createdAt' | 'updatedAt';
+
     @PrimaryKey()
     id!: number;
 
     @Property({ type: "date" })
     createdAt = new Date();
 
-    @Property({ type:'date', onUpdate: () => new Date() })
+    @Property({ type: "date", onUpdate: () => new Date() })
     updatedAt = new Date();
 
-    @Property({type: 'text'})
+    @Property({ type: "text" })
     title!: string;
 }
