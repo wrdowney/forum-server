9be6a9b4bf5485375f2d2a88b2804d2719c8100d
Will Downey <willd20@vt.edu>
2022-07-10 10:47:06 -0400

    feat(entities): Add User schema

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
 import path from 'path';
+import { User } from "./entities/User";
 
 export default {
     migrations: {
@@ -9,7 +10,7 @@ export default {
         pattern: /^[\w-]+\d+\.[tj]s$/,
     },
     allowGlobalContext: true,
-    entities: [Post],
+    entities: [Post, User],
     user: "postgres",
     password: "postgres",
     dbName: 'forum',
